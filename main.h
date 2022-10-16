78b11653e0f0e508c3c866234526a3d5c1ff7f7c
Shugo52 <olofuchubi@gmail.com>
2022-10-16 15:01:50 -0700

    another commit

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+void print_number(int n);
+void _puts(char *str);
+extern int count;
+
+#endif
