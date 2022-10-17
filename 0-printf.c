5d85f259fbf135ebb69701e3f4b227ced241e6c7
Adel ALX <a.adel.a.fattah@gmail.com>
2022-10-17 08:33:47 -0700

    main.c and betty

--- 0-printf.c
+++ 0-printf.c
@@ -8,9 +8,8 @@
  */
 int _printf(const char *format, ...)
 {
-	int i;
+	int i, count;
 	va_list next;
-	int count;
 
 	count = 0;
 	va_start(next, format);
@@ -22,32 +21,30 @@ int _printf(const char *format, ...)
 			{
 				_putchar(va_arg(next, int));
 				i++;
-				continue;
 			} else if (format[i + 1] == 's')
 			{
 				_puts(va_arg(next, char *));
 				i++;
-				continue;
 			} else if (format[i + 1] == '%')
 			{
 				_putchar('%');
 				i++;
 				count++;
-				continue;
 			} else if (format[i + 1] == 'd')
 			{
 				count += print_d(va_arg(next, int), 0);
 				i++;
-				continue;
 			} else if (format[i + 1] == 'i')
 			{
 				count += print_i(va_arg(next, int));
 				i++;
-				continue;
 			}
 		}
-		_putchar(format[i]);
-		count++;
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
 	}
 	va_end(next);
 	return (count);
