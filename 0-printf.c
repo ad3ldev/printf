2258ec7cb54e68a35f997fb5909612a28ef252cb
Shugo52 <olofuchubi@gmail.com>
2022-10-17 03:54:26 -0700

    checking

--- 0-printf.c
+++ 0-printf.c
@@ -36,6 +36,7 @@ int _printf(const char *format, ...)
 			}
 		}
 		_putchar(format[i]);
+		count++;
 	}
 	va_end(next);
 	return (count);
--- print_string.c
+++ print_string.c
@@ -2,16 +2,29 @@
 
 /**
  * _puts - Prints a string to stdout.
- * @str: Pointer to the string value to print.
+ * @new_string: Pointer to the string value to print.
  *
  * Return: None.
  */
-void _puts(char *str)
+void _puts(char *new_string)
 {
-	while (*str != '\0')
+	for (i = 0; new_string[i] != '\0'; i++)
 	{
-		_putchar(*str);
-		str++;
+		if (new_string[i] == '\' && new_string[i + 1] == '\')
+		{
+			_putchar('\')
+			i += 1;
+		} else if (new_string[i] == '\' && new_string[i + 1] == 'n')
+		{
+			_putchar('\n');
+			1 += 1;
+		} else if (new_string[i] == '\' && new_string[i + 1] == '"')
+		{
+			_putchar('\"');
+			i += 1;
+		} else
+		{
+			_putchar(new_string[i];
+		}
 	}
-	/* printf("\n"); */
 }
