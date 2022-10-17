977e6c3f92b2d2c1e888921655667d8adfe1758b
Shugo52 <olofuchubi@gmail.com>
2022-10-16 23:01:56 -0700

    check

---
+++ test/0-printf.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _printf - Prints output acording to an input format.
+ * @format: Pointer to a formated string
+ *
+ * Return: number of characters printed to the screen
+ */
+int _printf(const char *format, ...)
+{
+	int i;
+	va_list next;
+	int count;
+
+	count = 0;
+	va_start(next, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == 'c')
+			{
+				_putchar(va_arg(next, int));
+				i++;
+				continue;
+			} else if (format[i + 1] == 's')
+			{
+				_puts(va_arg(next, char *));
+				i++;
+				continue;
+			} else if (format[i + 1] == '%')
+			{
+				_putchar('%');
+				i++;
+				continue;
+			}
+		}
+		_putchar(format[i]);
+	}
+	va_end(next);
+	return (count);
+}
---
+++ test/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+void _puts(char *str);
+
+#endif
