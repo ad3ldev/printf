ebd27ffc7a19608868cf4ddf3e87d34b6fb8f9aa
Olofu Ojoachubione <87946002+Shugo52@users.noreply.github.com>
2022-10-18 08:08:53 +0100

    Update 0-printf.c

--- 0-printf.c
+++ 0-printf.c
@@ -17,8 +17,8 @@ int _printf(const char *format, ...)
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	/*if (format[0] == '%' && format[1] == '\0')
-			return (-1);*/
+	if (format[0] == '%' && format[1] == '\0')
+		return (-1);
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
