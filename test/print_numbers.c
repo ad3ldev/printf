d9fdfb76dbc0c9e5e6ce82f61b64456909bc4a20
Shugo52 <olofuchubi@gmail.com>
2022-10-18 02:27:27 -0700

    checking

--- test/print_numbers.c
+++ test/print_numbers.c
@@ -6,19 +6,26 @@
  * @count: counting of number;
  * Return: size of numnber
  */
-int print_d(int n, int count)
+int print_d(int n)
 {
+	int count;
+
+	count = 0;
 	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
 	}
-	if (n > 10)
+	if (n < 10)
 	{
-		count = print_d(n / 10, count);
+		_putchar(n % 10 + '0');
+		count++;
+	} else
+	{
+		print_d(n / 10);
+		_putchar(n % 10 + '0');
 	}
-	_putchar(n % 10 + '0');
-	return (count + 1);
+	return (count);
 }
 
 /**
@@ -28,5 +35,5 @@ int print_d(int n, int count)
  */
 int print_i(int n)
 {
-	return (print_d(n, 0));
+	return (print_d(n));
 }
