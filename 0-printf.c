0939e148b146b91fb0690b6f46b78890cc8a3d27
Olofu Ojoachubione <87946002+Shugo52@users.noreply.github.com>
2022-10-18 08:03:41 +0100

    Update 0-printf.c

--- 0-printf.c
+++ 0-printf.c
@@ -12,9 +12,13 @@ int _printf(const char *format, ...)
 	va_list next;
 
 	count = 0;
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 	va_start(next, format);
-	if (format[0] == '%' && format[1] == '\0')
-			return (-1);
+	/*if (format[0] == '%' && format[1] == '\0')
+			return (-1);*/
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
